b7ea3602ccfa1241fa75b39d9381ca6f4ee04ee6
Swamy Gopalsamy <swamy@homelight.com>
2017-10-20 13:57:59 -0700

    Remove properties that are returned only sometime and hence breaking [140]

--- lib/rubillow/models/updated_property_details.rb
+++ lib/rubillow/models/updated_property_details.rb
@@ -65,13 +65,7 @@ module Rubillow
           :current_month => @parser.xpath('//pageViewCount/currentMonth').first.text,
           :total => @parser.xpath('//pageViewCount/total').first.text
         }
-        @price = @parser.xpath('//price').first.text
-        @neighborhood = @parser.xpath('//neighborhood').first.text
-        @school_district = @parser.xpath('//schoolDistrict').first.text
-        @elementary_school = @parser.xpath('//elementarySchool').first.text
-        @middle_school = @parser.xpath('//middleSchool').first.text
-        @home_description = @parser.xpath('//homeDescription').first.text
-        
+
         @posting = {}
         @parser.xpath('//posting').children.each do |elm|
           @posting[underscore(elm.name).to_sym] = elm.text
